ecfd7e2c5290912fae529855f7609ef3089470fa
Praveen Sharma <praying.sharma@gmail.com>
2025-10-26 17:18:30 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Agent from "./pages/Agent";
+import Project from "./pages/Project";
+
+function App() {
+  return (
+    <>
+      <div className="text-white text-3xl">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/agent" element={<Agent />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
