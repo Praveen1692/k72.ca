890cd26e0e7d426b4dd36f36c0d5cc35c23bdbe4
Praveen Sharma <praying.sharma@gmail.com>
2025-10-28 23:25:09 +0530

    Agent Page

--- src/pages/Agent.jsx
+++ src/pages/Agent.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Agent() {
   return (
-    <div>Agent</div>
-  )
+    <div className="font-[font2]">
+      <div className="mt-[53vh]">
+        <h1 className="text-[19vw] text-center uppercase leading-[18vw]">
+          Soixan7e <br /> Douze
+        </h1>
+      </div>
+
+      <div className="pl-[40%]  mr-2  mt-20">
+        <p className="text-3xl ">
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+          Notre curiosité nourrit notre créativité. On reste humbles et on dit
+          non aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+          valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+          faire de bons chiffres à court terme, mais on la tue à long terme.
+          C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir
+          des marques influentes.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Agent
+export default Agent;
