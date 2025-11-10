a22788e37ba8185bfda637927c83fa3a581bd0e1
Praveen Sharma <praying.sharma@gmail.com>
2025-11-10 23:08:49 +0530

    Aget Page

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -10,7 +10,7 @@ function Video() {
         
         loop
         muted
-        src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1761670911~exp=1761674511~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=fbd92fb7796329faae39ce3579a48f1e273e0b7fae233bfd64cf46d572dc4374&r=dXMtZWFzdDE%3D"
+        src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1762795218~exp=1762798818~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=3201a45167c2e0b095b9f5dcd1f1ea794dd423f70fe3f780d58a3d481169fd3b&r=dXMtZWFzdDE%3D"
         className="h-full w-full object-cover"
       ></video>
     </div>
