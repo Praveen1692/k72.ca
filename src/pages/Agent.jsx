da0acabe35e07c02ed5f051c58d68ebe392724b7
Praveen Sharma <praying.sharma@gmail.com>
2025-11-10 23:45:25 +0530

    Agent Page Image

--- src/pages/Agent.jsx
+++ src/pages/Agent.jsx
@@ -1,33 +1,64 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
 
 function Agent() {
+  const imageDivRef = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  console.log("Image Div ref", imageDivRef.current);
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        top: "top 36%",
+        end: "top -80%",
+        scrub:true,
+        pin: true,
+
+      },
+    });
+  });
+
   return (
     <div>
-      <div className="h-[20vw] overflow-hidden  rounded-3xl -z-20    w-[15vw]  absolute top-30 left-[30vw]">
-        <img className="h-full w-full object-cover" src="https://k72.ca/images/teamMembers/MEGGIE_640X980_2.jpg?w=640&h=960&s=7d78cdb1fad347408e05a311cc4018ef" alt="img" />
-
-
-      </div>
-
-      <div className="font-[font2]">
-        <div className="mt-[55vh] z-auto">
-          <h1 className="text-[19vw] text-center uppercase leading-[18vw]">
-            Soixan7e <br /> Douze
-          </h1>
+      <div className="section1">
+        <div
+          ref={imageDivRef}
+          className="h-[20vw] overflow-hidden  rounded-3xl -z-20    w-[15vw]  absolute top-30 left-[30vw]"
+        >
+          <img
+            className="h-full w-full object-cover"
+            src="https://k72.ca/images/teamMembers/MEGGIE_640X980_2.jpg?w=640&h=960&s=7d78cdb1fad347408e05a311cc4018ef"
+            alt="img"
+          />
         </div>
 
-        <div className="pl-[40%]  mt-20">
-          <p className="text-3xl ">
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
-            Notre curiosité nourrit notre créativité. On reste humbles et on dit
-            non aux gros egos, même le vôtre. Une marque est vivante. Elle a des
-            valeurs, une personnalité, une histoire. Si on oublie ça, on peut
-            faire de bons chiffres à court terme, mais on la tue à long terme.
-            C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir
-            des marques influentes.
-          </p>
+        <div className="font-[font2]">
+          <div className="mt-[55vh] z-auto">
+            <h1 className="text-[19vw] text-center uppercase leading-[18vw]">
+              Soixan7e <br /> Douze
+            </h1>
+          </div>
+
+          <div className="pl-[40%]  mt-20">
+            <p className="text-3xl ">
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;
+              &nbsp; Notre curiosité nourrit notre créativité. On reste humbles
+              et on dit non aux gros egos, même le vôtre. Une marque est
+              vivante. Elle a des valeurs, une personnalité, une histoire. Si on
+              oublie ça, on peut faire de bons chiffres à court terme, mais on
+              la tue à long terme. C’est pour ça qu’on s’engage à donner de la
+              perspective, pour bâtir des marques influentes.
+            </p>
+          </div>
         </div>
       </div>
+
+      <div className="section2"></div>
     </div>
   );
 }
